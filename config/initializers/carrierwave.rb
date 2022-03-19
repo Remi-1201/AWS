9bd7bebd046b17d8c8a4d0ce77ddad792ab4fa9a
remi-1201 <remi.nkjm@gmail.com>
2022-03-19 15:52:48 +0900

    fix some errors

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -24,3 +24,4 @@ CarrierWave.configure do |config|
       config.fog_directory = 'achieve-development'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-development'
 end
+end
